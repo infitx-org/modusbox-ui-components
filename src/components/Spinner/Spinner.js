10f372b03ed8b97ba27d0849354d8bb20cff221d
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2018-03-14 17:59:39 +0100

    Spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,54 @@
+import React, { PropTypes } from 'react'
+import './Spinner.css'
+
+
+const polarToCartesian = ( centerX, centerY, radius, angleInDegrees ) => {
+  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+const describeArc = ( x, y, radius, startAngle, endAngle ) => {
+
+    const start = polarToCartesian(x, y, radius, endAngle);
+    const end = polarToCartesian(x, y, radius, startAngle);
+    const largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+    return [
+        'M', start.x, start.y, 
+        'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(' ');    
+}
+
+const Spinner = ({ size, center }) => {
+  
+  const strokeWidth = size / 10
+  const width = `${size}px`, height = `${size}px`
+  const position = size/2
+  const radius = position - strokeWidth
+
+  return(
+    <div className={`modus-spinner ${center ? 'center' : '' }`}>
+      <svg 
+        className='modus-spinner__component'
+        width={ width }
+        height={ height }
+        viewBox={`0 0 ${size} ${size}`}
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path 
+          id='p1'
+          className='path'
+          fill='none'
+          strokeWidth={ strokeWidth }
+          d={ describeArc( position, position, radius, 90, 200 )}
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default Spinner
+
+
---
+++ src/views/TestSpinner.js
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react'
+import Spinner from '../components/Spinner'
+
+const Svg = () => (
+	<div>
+		<style>
+		</style>
+		<div style={{padding:10, margin:'5px 0px', border: '1px solid #ccc'}}>
+			<Spinner size={ 10 }/>
+			<Spinner size={ 20 }/>
+			<Spinner size={ 30 }/>
+			<Spinner size={ 40 }/>
+			<Spinner size={ 50 }/>
+			<Spinner size={ 60 }/>
+		</div>
+		<div style={{height: '100px', width: '100px', border: '1px solid #ccc'}}>
+			<Spinner size={ 50 } center/>
+		</div>
+	</div>
+)
+
+export default Svg
+
