00b66e1b913832f2f7cb516ed94ff9db8a721541
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2018-04-11 18:20:22 +0200

    Update postinstall scripts

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,9 +2,13 @@
 var path = require('path');
 var exec = require('child_process').exec;
 var cwd = process.cwd();
-if (cwd.indexOf('@') === -1) {
+
+var webpackPath = path.join(cwd, 'node_modules', '.bin', 'webpack');
+if (cwd.indexOf('node_modules') !== -1) {
+	webpackPath = path.join(cwd, '..','.bin','webpack');
+}	
+	
 	console.log('Building components');
-	var webpackPath = path.join(cwd, 'node_modules', '.bin', 'webpack');
 	var command = webpackPath + ' --config webpack.production.config.js --inline --bail';
 	exec(command, { cwd: cwd }, function(error, stdout, stderr) {
 		if (error) {
@@ -12,6 +16,4 @@ if (cwd.indexOf('@') === -1) {
 		}
 		console.log('Built Components');
 	});
-} else {
-	console.log('in node_modules context, stop DLL build on postinstall');
-}
+
