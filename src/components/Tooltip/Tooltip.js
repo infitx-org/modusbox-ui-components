007772ae2d5eac876b987f5091d2722c4d9b0109
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2018-04-12 17:53:37 +0200

    Autoposition tooltip component

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -17,35 +17,73 @@ class Tooltip extends PureComponent {
 	}
 	componentDidUpdate() {}
 	componentWillUnmount() {
+		this.hideTooltip();
 		this.box.removeEventListener('mouseenter', this.showTooltip);
 		this.box.removeEventListener('mouseleave', this.hideTooltip);
 	}
 	showTooltip() {
-		if (this.box) {
-			const { scrollWidth, offsetWidth } = this.box;
-
-			if (scrollWidth > offsetWidth) {
-				this.hasTooltip = true;
-
-				const { left, top } = this.box.getBoundingClientRect();
-				const tooltipTop = top - 40;
-				const CMP = () => <span>{this.props.children}</span>;
-
-				this._div = document.createElement('div');
-				this._div.className = 'element-tooltip__viewer';
-				this._div.style.top = tooltipTop;
-				this._div.style.left = left;
-				this._target = document.body.appendChild(this._div);
-				this._component = renderSubtreeIntoContainer(this, <CMP />, this._target);
-			}
+		if (this.box === undefined) {
+			return;
 		}
+
+		const { content, label, children } = this.props;
+		const { scrollWidth, offsetWidth } = this.box;
+		const shouldShowTooltip = scrollWidth > offsetWidth || label !== undefined || content !== undefined;
+
+		if (!shouldShowTooltip) {
+			return;
+		}
+
+		let cmp = <span>{children}</span>;
+		let defaultAppearance = true;
+		if (content) {
+			cmp = content;
+			defaultAppearance = false;
+		} else if (label) {
+			cmp = <span>{label}</span>;
+		}
+
+		this._div = document.createElement('div');
+		this._div.className = 'element-tooltip__viewer';
+		if (defaultAppearance === true) {
+			this._div.className += ' element-tooltip__viewer--default';
+		}
+		this._target = document.body.appendChild(this._div);
+		this._component = renderSubtreeIntoContainer(this, cmp, this._target);
+
+		// Get component sizes
+		const { innerWidth } = window;
+		const boxRect = this.box.getBoundingClientRect();
+		const tooltipRect = this._target.getBoundingClientRect();
+
+		// Calculate tooltip position, it should be centered on top of the box
+		let top = boxRect.top - tooltipRect.height - 10;
+		let left = boxRect.left + ((boxRect.width - tooltipRect.width) / 2);
+
+		if (left < 10) {
+			left = 10;
+		}
+		if (left + tooltipRect.width > (innerWidth - 10)) {
+			left = innerWidth - 10 - tooltipRect.width;
+		}
+		if (top < 10) {
+			top = top + boxRect.height + 10;
+		}
+
+		// Apply final updates to the tooltip itself
+		this._target.style.top = top;
+		this._target.style.left = left;
+		this._target.className += ' element-tooltip__viewer--fade-in';
+
+		this._hasMountedTooltip = true;
 	}
 	hideTooltip() {
-		if (this.hasTooltip) {
+		if (this._hasMountedTooltip) {
 			ReactDOM.unmountComponentAtNode(this._target);
 			document.body.removeChild(this._target);
 			this._target = null;
 			this._component = null;
+			this._hasMountedTooltip = false;
 		}
 	}
 
@@ -66,12 +104,16 @@ class Tooltip extends PureComponent {
 }
 
 Tooltip.propTypes = {
+	content: PropTypes.node,
 	children: PropTypes.node,
 	style: PropTypes.shape(),
+	label: PropTypes.string,
 };
 Tooltip.defaultProps = {
+	content: undefined,
 	children: null,
 	style: {},
+	label: undefined,
 };
 
 export default Tooltip;
