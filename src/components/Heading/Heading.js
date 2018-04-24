0aacf865a900598329343bad2cd8167d306123cc
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2018-04-24 17:05:12 +0200

    Span in Heading html components

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -8,13 +8,13 @@ class Heading extends PureComponent {
 		const number = Number.isNaN(intSize) === false && intSize;
 		let component = null;
 		switch (number) {
-		case 1: component = <h1 style={style}>{children}</h1>; break;
-		case 2: component = <h2 style={style}>{children}</h2>; break;
-		case 3: component = <h3 style={style}>{children}</h3>; break;
-		case 4: component = <h4 style={style}>{children}</h4>; break;
-		case 5: component = <h5 style={style}>{children}</h5>; break;
-		case 6: component = <h6 style={style}>{children}</h6>; break;
-		default: component = <h3 style={style} >{children}</h3>; break;
+		case 1: component = <h1 style={style}><span>{children}</span></h1>; break;
+		case 2: component = <h2 style={style}><span>{children}</span></h2>; break;
+		case 3: component = <h3 style={style}><span>{children}</span></h3>; break;
+		case 4: component = <h4 style={style}><span>{children}</span></h4>; break;
+		case 5: component = <h5 style={style}><span>{children}</span></h5>; break;
+		case 6: component = <h6 style={style}><span>{children}</span></h6>; break;
+		default: component = <h3 style={style}><span>{children}</span></h3>; break;
 		}
 		return component;
 	}
