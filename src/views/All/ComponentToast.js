87df473f639359a524a5c64d1131ba6d1e4d6413
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2018-05-22 21:03:08 +0200

    Fix Toast view (#32)

--- src/views/All/ComponentToast.js
+++ src/views/All/ComponentToast.js
@@ -1,74 +1,85 @@
 import React, { Component } from 'react';
 
-import Column from '../../components/Column';
+import Row from '../../components/Row';
 import Toast from '../../components/Toast';
 import Button from '../../components/Button';
 
+function showErrorToast() {
+  Toast.show({
+    kind: 'error',
+    title: 'show me error toast',
+  });
+}
+function showInfoToast() {
+  Toast.show({
+    kind: 'info',
+    title: 'show me info toast',
+  });
+}
+function showCloseableInfoToast() {
+  Toast.show({
+    kind: 'info',
+    closeable: true,
+    title: 'show me closeable info toast',
+  });
+}
+function showSuccessToast() {
+  Toast.show({
+    element: <Toast title="Success Toast!" kind="success" />,
+  });
+}
+function showCustomToast() {
+  Toast.show({
+    element: (
+      <div>
+        <span>This is a custom Toast!</span>
+        <img
+          width="200"
+          height="160"
+          alt="Toast!"
+          // eslint-disable-next-line max-len
+          src="https://www.ahealthiermichigan.org/wp-content/uploads/2014/09/Transform-toast-into-breakfast.jpg"
+        />
+      </div>
+    ),
+  });
+}
 export default class TestToast extends Component {
-  static showErrorToast() {
-    Toast.show({
-      kind: 'error',
-      title: 'show me error toast',
-    });
-  }
-  static showInfoToast() {
-    Toast.show({
-      kind: 'info',
-      title: 'show me info toast',
-    });
-  }
-  static showCloseableInfoToast() {
-    Toast.show({
-      kind: 'info',
-      closeable: true,
-      title: 'show me closeable info toast',
-    });
-  }
-  static showSuccessToast() {
-    Toast.show({
-      element: <Toast title="Success Toast!" kind="success" />,
-    });
-  }
-  static customFunction() {
-    // eslint-disable-next-line no-console
-    console.log('alert');
-  }
-  showCustomToast() {
-    Toast.show({
-      element: (
-        <Column align="center" style={{ background: '#CACBCC', padding: 20, width: 400 }}>
-          <span>MMMMMMMM</span>
-          <img
-            width="200"
-            height="160"
-            alt="Toast!"
-            // eslint-disable-next-line max-len
-            src="https://www.ahealthiermichigan.org/wp-content/uploads/2014/09/Transform-toast-into-breakfast.jpg"
-          />
-          <Button style={{ marginTop: 10 }} onClick={this.customFunction}>
-            Gimme the Toast!
-          </Button>
-        </Column>
-      ),
-    });
-  }
   render() {
+    const buttonStyle = { margin: '5px' };
     return (
-      <Column align="left space-around" height="300">
-        <Button label="Click to Show Info Toast" kind="secondary" onClick={this.showInfoToast} />
+      <Row align="left">
         <Button
-          label="Click to Show Closeable Info Toast"
-          kind="secondary"
-          onClick={this.showCloseableInfoToast}
+          style={buttonStyle}
+          label="Info Toast"
+          kind="primary"
+          onClick={showInfoToast}
         />
-        <Button label="Click to Show Error Toast" kind="danger" onClick={this.showErrorToast} />
         <Button
-          label="Click to Show Success Toast"
+          style={buttonStyle}
+          label="Closeable Info Toast"
           kind="secondary"
-          onClick={this.showSuccessToast}
+          onClick={showCloseableInfoToast}
         />
-        <Button label="Click to Show Custom Toast" kind="primary" onClick={this.showCustomToast} />
-      </Column>
+        <Button
+          style={buttonStyle}
+          label="Error Toast"
+          kind="danger"
+          onClick={showErrorToast}
+        />
+        <Button
+          style={buttonStyle}
+          label="Success Toast"
+          kind="secondary"
+          onClick={showSuccessToast}
+        />
+        <Button
+          style={buttonStyle}
+          label="Custom Toast"
+          kind="warning"
+          onClick={showCustomToast}
+        />
+      </Row>
     );
   }
 }
