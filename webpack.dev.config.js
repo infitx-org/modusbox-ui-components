5162e986127fde455e4aea7a3534e6a92ac68888
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2018-06-15 14:48:55 +0200

    Emit warnings in eslint-loader

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -28,6 +28,7 @@ module.exports = {
             loader: 'eslint-loader',
             options: {
               fix: true,
+              emitWarnings: true,
             },
           },
         ],
