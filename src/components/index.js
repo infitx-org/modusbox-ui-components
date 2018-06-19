590e68cdf676677d7322df9b893b5d757d70b29b
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2018-06-19 14:29:09 +0200

    export Menu component(s)

--- src/components/index.js
+++ src/components/index.js
@@ -12,6 +12,7 @@ import FileUploader from './FileUploader';
 import Heading from './Heading';
 import Icon from './Icon';
 import Modal, { ModalTabsLayout } from './Modal';
+import Menu, { MenuSection, MenuItem } from './Menu';
 import RadioGroup from './RadioGroup';
 import Row from './Row';
 import ScrollBox from './ScrollBox';
@@ -30,6 +31,9 @@ export {
   FileUploader,
   Heading,
   Icon,
+  Menu,
+  MenuSection,
+  MenuItem,
   Modal,
   ModalTabsLayout,
   RadioGroup,
