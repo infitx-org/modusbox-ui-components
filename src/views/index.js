2e445c0dace2b070dc73491d0a21c47451cdb84f
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2019-10-18 17:31:42 +0200

    New viewer menu based

--- src/views/index.js
+++ src/views/index.js
@@ -1,78 +1,82 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import { Tab, Tabs, TabList, TabPanels, TabPanel } from '../components/Tabs';
+import Menu, { MenuItem } from '../components/Menu';
 import ScrollBox from '../components/ScrollBox';
+import Heading from '../components/Heading';
 import * as ComponentViews from './All';
 import WrappedNavbar from './All/ComponentNavbar';
-
 import '../assets/styles/index.scss';
+import './index.css';
+
 
 const componentMappings = Object.keys(ComponentViews).map(view => ({
   name: view.substring(9), // 'Remove the prefix "component"
   view,
 }));
+const viewNames = componentMappings.map(c => c.name);
 
-const AllItemTabs = componentMappings.map(({ name }) => <Tab key={name}> {name} </Tab>);
-const AllItemPanels = componentMappings.map(({ view, name }) => {
-  const View = ComponentViews[view];
+const MenuItems = componentMappings.map(({ name }) => (
+  <MenuItem key={name} path={`/${name}`} label={name}>{name}</MenuItem>
+));
+
+const Views = {}
+
+componentMappings.forEach(({ view, name }) => {
+  const CMP = ComponentViews[view]
   if (name.includes('DataList') || name.includes('ContentReader')) {
-    return (
-      <TabPanel key={name}>
-        <View />
-      </TabPanel>
-    );
-  }
-  return (
-    <TabPanel key={name}>
+    Views[name] = <CMP />;
+  } else {
+    Views[name] = (
       <ScrollBox>
-        <View />
+        <CMP />
       </ScrollBox>
-    </TabPanel>
-  );
+    )
+  }
 });
 
-class Views extends React.Component {
+class Examples extends React.Component {
   constructor(props) {
     super(props);
     this.onSelectTab = this.onSelectTab.bind(this);
-    const selectedTab = parseInt(window.localStorage.getItem('tab') || 0, 10);
-    const tab = selectedTab !== undefined ? selectedTab : componentMappings.length - 1;
+    
+    const selectedTab = window.localStorage.getItem('tab');
+    let tab;
+    if (viewNames.includes(selectedTab)) {
+      tab = selectedTab;
+    } else {
+      tab = `${viewNames[0]}`;
+    }
 
     this.state = {
       tab,
     };
   }
-  onSelectTab(e, tab) {
-    window.localStorage.setItem('tab', tab);
-    this.setState({ tab });
+  onSelectTab(tab) {
+    const viewName = tab.substring(1);
+    window.localStorage.setItem('tab', viewName);
+    this.setState({ tab: viewName });
   }
 
   render() {
     return (
-      <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+      <div id="navbar">
         <WrappedNavbar />
-        <div
-          style={{
-            position: 'absolute',
-            top: '55px',
-            width: '100%',
-            bottom: '0px',
-          }}
-        >
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              bottom: 0,
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <Tabs selected={this.state.tab} onSelect={this.onSelectTab} flex id="main-tabs">
-              <TabList>{AllItemTabs}</TabList>
-              <TabPanels>{AllItemPanels}</TabPanels>
-            </Tabs>
+        <div id="content">
+          <div id="menu">
+            <ScrollBox flex>
+              <Menu path="/" pathname={`/${this.state.tab}`} onChange={this.onSelectTab}>
+                {MenuItems}
+              </Menu>
+            </ScrollBox>
+          </div>
+          <div id="view">
+
+            <div id="view__header">
+              <Heading size='2'>{this.state.tab}</Heading>
+            </div>
+            <div id="view__content">
+              {Views[this.state.tab]}
+            </div>
           </div>
         </div>
       </div>
@@ -80,4 +84,4 @@ class Views extends React.Component {
   }
 }
 
-export default hot(Views);
+export default hot(Examples);
