f7f6863af49d8c7300df6e6fd051189fa6d3020d
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2020-03-10 16:25:40 +0100

    Improve card, import icon

--- src/components/FormInputs/components.js
+++ src/components/FormInputs/components.js
@@ -1,4 +1,5 @@
 import './FormInput.scss';
+import '../../icons/mule/forum-small.svg';
 
 import React, { PureComponent } from 'react';
 
@@ -6,6 +7,7 @@ import * as utils from '../../utils/common';
 import Button from '../Button';
 import Icon from '../Icon';
 import Tooltip from '../Tooltip';
+
 
 export const FieldInfoOverlay = ({ assignRef, title, description, url, required }) => (
   <div className="forminput__field-info" ref={assignRef}>
