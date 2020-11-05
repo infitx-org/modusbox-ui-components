cfbbbf209cef0a17ceee876c6286f3b48ab1e372
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2020-11-05 18:12:41 +0100

    Make injector a store enhancer; make inject return the sliced state instead of the location

--- src/rredux/hooks/index.ts
+++ src/rredux/hooks/index.ts
@@ -1,35 +1,23 @@
 import { useState, useEffect } from 'react';
 import { Reducer } from 'redux';
 import { useStore } from 'react-redux';
+import { Saga } from '@redux-saga/types';
 import { InjectableStore } from '../injectors/types';
 
-type Saga = () => Generator;
-
 // Installs the reducer on the parent app and makes sure it is used
-const useReducerLoader = (reducerFn: Reducer, saga: Saga) => {
+const useReducerLoader = (reducer: Reducer, saga: Saga) => {
   const store = useStore() as InjectableStore;
-  const [ready, setReady] = useState(false);
-  const [clone, setClone] = useState(store);
+  const [clone, setClone] = useState(null);
 
   useEffect(() => {
     function injectAndGetStoreInstance() {
-      const assignedPath = store.injectReducerAndSaga(reducerFn, saga);
-      setClone(
-        Object.assign(Object.create(Object.getPrototypeOf(store)), store, {
-          getState: function getState() {
-            return store.getState()[assignedPath];
-          },
-        }),
-      );
-      setReady(true);
+      const assignedPath = store.inject({ reducer, saga });
+      setClone(assignedPath);
     }
     injectAndGetStoreInstance();
   }, []);
 
-  if (ready) {
-    return clone;
-  }
-  return false;
+  return clone;
 };
 
 export { useReducerLoader };
